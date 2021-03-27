8fa7005b797a05607c469cee7870fb4955465cf1
Marco Sarti <marco.sarti@plansoft.it>
2021-03-28 00:33:58 +0100

    fixed memory test

--- solutions/java-memory/Main.java
+++ solutions/java-memory/Main.java
@@ -2,5 +2,9 @@ class Main {
     public static void main(String[] args) {
         long heapMaxSize = Runtime.getRuntime().maxMemory();
         System.out.println("max memory is "+heapMaxSize);
+
+        int size = 200*1024*1024;
+
+        byte[] buffer = new byte[size];
     }
 }
